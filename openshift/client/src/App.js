db531ece8b1ff6c0ac416528d815b2e55767f5f8
bcgov-brwang <bcgov.brwang@gmail.com>
2023-01-12 15:02:21 -0800

    updated

--- openshift/client/src/App.js
+++ openshift/client/src/App.js
@@ -1,32 +1,29 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, Component, setState} from "react";
 import './App.css';
 import axios from 'axios';
 
 function App() {
   const [data, setData] = useState({ quotes: [] });
-  const apiUrl = "http://localhost:3000/quotes";
+  const apiUrl = "http://localhost:8000/quotes";
 
 
   useEffect(() => {
     async function fetchData() {
       // You can await here
       const result = await axios(
-        //'https://hn.algolia.com/api/v1/search?query=redux',
         apiUrl,
       );
-      setData(result.data);
-      // ...
+      setData({quotes: result.data.data});   
     }
     fetchData();
   }, []);
 
 
-
   return (
     <ul>
       {data.quotes.map(item => (
-        <li key={item.objectID}>
-          <a href={item.url}>{item.title}</a>
+        <li key={item.id}>
+          <a href={item.author}>{item.quote}</a>
         </li>
       ))}
     </ul>
--- openshift/server/config.js
+++ openshift/server/config.js
@@ -3,7 +3,7 @@ const env = process.env;
 const config = {
   db: { /* do not put password or any sensitive info here, done only for demo */
     host: env.DB_HOST || 'localhost',
-    port: env.DB_PORT || '5432',
+    port: env.DB_PORT || '5433',
     user: env.DB_USER || 'postgres',
     password: env.DB_PASSWORD || 'postgres',
     database: env.DB_NAME || 'postgres',
